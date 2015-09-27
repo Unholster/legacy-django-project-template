daab5d8c51991e056c1271ea0046199ac4424f7d
Sebastián Acuña <sacuna@gmail.com>
2015-09-27 11:28:14 -0300

    Include default 'index' url, that returns a 404

--- project_name/conf/urls.py
+++ project_name/conf/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns(
     '',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'django.views.defaults.page_not_found', name='index')
 )
